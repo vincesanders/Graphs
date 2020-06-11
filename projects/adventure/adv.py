00d5eb7defa2ae06921420a06ec53880a45a45fc
Vincent Sanders <vincesanders@hotmail.com>
2020-06-11 14:34:14 -0500

    Second pass solution.
    996 moves

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -5,6 +5,19 @@ from world import World
 import random
 from ast import literal_eval
 
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 class Stack():
     def __init__(self):
         self.stack = []
@@ -23,11 +36,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "projects/adventure/maps/test_line.txt"
+# map_file = "projects/adventure/maps/test_line.txt"
 # map_file = "projects/adventure/maps/test_cross.txt"
 # map_file = "projects/adventure/maps/test_loop.txt"
 # map_file = "projects/adventure/maps/test_loop_fork.txt"
-# map_file = "projects/adventure/maps/main_maze.txt"
+map_file = "projects/adventure/maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -42,15 +55,10 @@ player = Player(world.starting_room)
 visited = {}
 visited[player.current_room.id] = True
 
-print(player.current_room.get_exits())
-print(world.rooms[0].get_room_in_direction('n'))
-newRoom = world.rooms[0].get_room_in_direction('n').id
-print(newRoom)
-print(len(world.rooms))
-
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+rooms_to_visit = Stack()
 
 def traverse():
     found_exit = True
@@ -58,47 +66,55 @@ def traverse():
         found_exit = False
         exits = player.current_room.get_exits()
         # go to first unexplored (?) direction
+        current = player.current_room
         for direction in exits:
-            if player.current_room.get_room_in_direction(direction).id not in visited:
-                player.travel(direction)
-                traversal_path.append(direction)
-                visited[player.current_room.id] = True
-                found_exit = True
-                break
+            if current.get_room_in_direction(direction).id not in visited:
+                if found_exit:
+                    rooms_to_visit.push(current.get_room_in_direction(direction).id)
+                else:
+                    player.travel(direction)
+                    traversal_path.append(direction)
+                    visited[player.current_room.id] = True
+                    found_exit = True
         # loop until no unexplored direction
 
-def find_unexplored():
-    opposite_direction = {'n': 's', 's': 'n', 'w': 'e', 'e': 'w'}
-    backward_path = []
-    index = len(traversal_path) - 1
-    found_new_path = False
-    while index >= 0 and not found_new_path:
-        # walk backward through traversal_path
-        direction = opposite_direction[traversal_path[index]]
-        index -= 1
-        player.travel(direction)
-        # add directions to path as you traverse
-        backward_path.append(direction)
-        # check for unexplored exits at each room
-        exits = player.current_room.get_exits()
-        for d in exits:
-            # go to the first unexplored room
-            if player.current_room.get_room_in_direction(d).id not in visited:
-                # we found an unexplored room
-                player.travel(d)
-                # extend traversal path with backward path
-                traversal_path.extend(backward_path)
-                traversal_path.append(d)
-                visited[player.current_room.id] = True
-                found_new_path = True
-                break
+def find_shortest_path_to_unexplored():
+    visited_room = set()
 
-while len(world.rooms) != len(visited):
+    q = Queue()
+    q2 = Queue()
+
+    q.enqueue([])
+    q2.enqueue(player.current_room)
+    destination = rooms_to_visit.pop()
+
+    while q.size() > 0:
+        path = q.dequeue()
+        current = q2.dequeue()
+        if current.id not in visited_room:
+            visited_room.add(current.id)
+            if current.id == destination:
+                return path
+            exits = current.get_exits()
+            for direction in exits:
+                path_copy = list(path)
+                path_copy.append(direction)
+                q.enqueue(path_copy)
+                q2.enqueue(current.get_room_in_direction(direction))
+    return None
+
+def find_unexplored(path):
+    # move player along path to unexplored
+    for direction in path:
+        player.travel(direction)
+        traversal_path.append(direction)
+    visited[player.current_room.id] = True
+
+while len(world.rooms) > len(visited):
     traverse()
-    # use bft to find the shortest path back to an unexplored direction
-    # If visited == number of rooms, all rooms are explored 
     if len(visited) != len(world.rooms):
-        find_unexplored()
+        path = find_shortest_path_to_unexplored()
+        find_unexplored(path)
 
 
 
